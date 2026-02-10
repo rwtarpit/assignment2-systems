97d2993022a3fa6030e3a89737585bbc5ddc3b4b
Arpit Rawat <arpitrawat0404@gmail.com>
2026-02-10 19:50:09 +0000

    correct autograd flash attention

--- cs336_systems/flash_attention/autograd.py
+++ cs336_systems/flash_attention/autograd.py
@@ -15,35 +15,36 @@ class FlashAttention(Function):
         
         tile_size = 16
         final_output = torch.empty_like(q)
-        final_logsum_exp = torch.empty(tile_size)
+        final_logsum_exp = torch.empty((q.shape[0],q.shape[1]))
         tile_q = torch.arange(0,q.shape[1],tile_size)
         d_model = q.shape[-1]
         
         for i in tile_q:
-            current_q = q[...,i:i+tile_size,...] # (batch,tile_size,d_model)
+            current_q = q[:,i:i+tile_size,:] # (batch,tile_size,d_model)
             current_tile_size = current_q.shape[1]
             tile_output = torch.zeros_like(current_q) # (batch,tile_size,d_model)
-            norm_factor = torch.zeros(q.shape[0],current_tile_size)
+            norm_factor = torch.zeros(q.shape[0],current_tile_size) # (batch,tile_size)
             max_el = torch.full((q.shape[0],current_tile_size),-torch.inf) # (batch,tile_size)
             
             for j in tile_q:
-                current_k = k[...,j:j+tile_size,...] #(batch,tile_size,d_model)
-                current_v = v[...,j:j+tile_size,...] #(batch,tile_size,d_model)
+                current_k = k[:,j:j+tile_size,:] #(batch,tile_size,d_model)
+                current_v = v[:,j:j+tile_size,:] #(batch,tile_size,d_model)
                 
-                mat_mul = (current_q@current_k.permute(0,2,1))/d_model # (batch,tile_size,tile_size)
-                print(mat_mul.shape)
+                mat_mul = (current_q@current_k.permute(0,2,1))/d_model**0.5 # (batch,tile_size,tile_size)
                 row_max = torch.max(mat_mul,dim=-1).values # (batch,tile_size)
                 last_max_el = max_el
                 max_el = torch.maximum(max_el,row_max) #(batch,tile_size) #we want to braodcast max_el over all elements in the tile
                 numer = torch.exp(mat_mul - max_el.unsqueeze(-1))  # (batch,tile_size,tile_size)
-                norm_factor =  torch.exp(last_max_el-max_el)*norm_factor + torch.sum(numer,dim=-1)
-                tile_output[...,:j,...] = torch.diag(torch.exp(last_max_el-max_el))*tile_output # reslolving previous tiles
-                tile_output[...,j:j+tile_size,...] = numer@current_v # updating latest output tile
+                
+                rescale_factor = torch.exp(last_max_el - max_el) # (batch,tile_size)
+                norm_factor =  rescale_factor*norm_factor + torch.sum(numer,dim=-1)
+                tile_output = tile_output*rescale_factor.unsqueeze(-1) + numer@current_v # reslolving previous tiles
 
-            final_output[i:i+tile_size,...] = tile_output * (torch.diag(1/norm_factor)) # (tile_size,d_model)
-            final_logsum_exp[i:i+tile_size] = max_el + torch.log(norm_factor) #(tile_size)
+            final_output[:,i:i+tile_size,:] = tile_output/norm_factor.unsqueeze(-1)   # (batch,tile_size,d_model)
+            final_logsum_exp[:,i:i+tile_size] = max_el + torch.log(norm_factor) #(batch,tile_size)
             
-        return final_output,final_logsum_exp
+        ctx.save_for_backward(q, k, v, final_output, final_logsum_exp)
+        return final_output
     
     
     def backward():
